21299167e8bfcc52f1bc26957be14d53f4f8658b
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-07-22 14:29:42 +0100

    Improve formating

--- components/Contacts.js
+++ components/Contacts.js
@@ -5,12 +5,12 @@ var Contacts = React.createClass({
 
 	render: function() {
 		var contacts = this.props.items.map(function(contact) {
-			return <Contact item={contact} key={contact.id}</Contact>
+			return <Contact item={contact} key={contact.id}<Contact/>
 		});
 		return (
-		<ul className={'contactsList'}>
-		{contacts}
-		</ul>
+			<ul className={'contactsList'}>
+			{contacts}
+			<ul/>
 		);
 	}
 });
